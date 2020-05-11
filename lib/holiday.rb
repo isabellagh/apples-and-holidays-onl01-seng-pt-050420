b52f82228a892100e41fa8d1976fac4c539a8468
isabella g henriques <isabellagh88@gmail.com>
2020-05-11 14:56:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,12 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.collect do |seasons, holidays|
+    puts "#{seasons.capitalize}:"
+    holidays.map do |holiday, supply|
+      puts "  #{holiday.to_s.split("_").each{|letters| letters.capitalize!}.join(' ')}: #{supply.join(", ")}"
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
