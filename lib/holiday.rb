ca5dd0de42adbea2d46a0f3d4099a696b1deb38e
isabella g henriques <isabellagh88@gmail.com>
2020-05-11 14:54:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,6 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash[:winter].collect do |holidays, items|
     items << supply
+  end
 end
 
 
